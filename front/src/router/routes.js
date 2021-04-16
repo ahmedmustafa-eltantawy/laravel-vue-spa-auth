9c787c2b0b9f05d3d3eb6a6bc379665dcfeeec43
Ahmed Mostafa Eltantawy <ahmedmostafaui@gmail.com>
2021-04-16 11:50:16 +0200

    Add Vue-Router

---
+++ front/src/router/routes.js
@@ -0,0 +1,15 @@
+const routes = [
+	{
+		path: "/login",
+		name: "login",
+		component: () => import("../views/auth/login.vue"),
+	},
+
+	{
+		path: "/register",
+		name: "register",
+		component: () => import("../views/auth/register.vue"),
+	},
+];
+
+export default routes;
