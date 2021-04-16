086ba3fbb7fd2d97d0a2b9bc3a70e17d1f851033
Ahmed Mostafa Eltantawy <ahmedmostafaui@gmail.com>
2021-04-16 11:48:14 +0200

    Add Login/Register Views

---
+++ front/src/views/auth/login.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<container classes="set-min-height">
+			<template #content>
+				<login-form></login-form>
+			</template>
+		</container>
+		<the-footer></the-footer>
+	</div>
+</template>
+
+<script>
+	import container from "../../components/ContainerFrontPages";
+	import LoginForm from "../../components/auth/LoginForm";
+	import TheFooter from "../../components/auth/TheFooter";
+	export default {
+		name: "login",
+		components: {
+			container,
+			LoginForm,
+			TheFooter,
+		},
+
+		data() {
+			return {};
+		},
+	};
+</script>
---
+++ front/src/views/auth/register.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<container classes="set-min-height">
+			<template #content>
+				<registration-form></registration-form>
+			</template>
+		</container>
+		<the-footer></the-footer>
+	</div>
+</template>
+
+<script>
+	import container from "../../components/ContainerFrontPages";
+	import RegistrationForm from "../../components/auth/RegistrationForm";
+	import TheFooter from "../../components/auth/TheFooter";
+	export default {
+		name: "register",
+		components: {
+			container,
+			RegistrationForm,
+			TheFooter,
+		},
+
+		data() {
+			return {};
+		},
+	};
+</script>
