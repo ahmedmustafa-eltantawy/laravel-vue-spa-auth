16220ba91ef6a45cbf3f8a1fa7a8493f98e0795c
Ahmed Mostafa Eltantawy <ahmedmostafaui@gmail.com>
2021-04-17 08:48:33 +0200

    Add User + Dashboard Routes + Gaurd funs

--- front/src/router/routes.js
+++ front/src/router/routes.js
@@ -1,14 +1,63 @@
+const guest = (to, from, next) => {
+	if (!localStorage.getItem("authToken")) {
+		return next();
+	} else {
+		return next("/");
+	}
+};
+const auth = (to, from, next) => {
+	if (localStorage.getItem("authToken")) {
+		return next();
+	} else {
+		return next("/login");
+	}
+};
+
 const routes = [
+	{
+		path: "/",
+		name: "home",
+		component: () => import("../views/home.vue"),
+	},
 	{
 		path: "/login",
 		name: "login",
+		beforeEnter: guest,
 		component: () => import("../views/auth/login.vue"),
 	},
 
 	{
 		path: "/register",
 		name: "register",
+		beforeEnter: guest,
 		component: () => import("../views/auth/register.vue"),
+	},
+
+	{
+		path: "/logout",
+		name: "logout",
+		beforeEnter: auth,
+		component: () => import("../views/auth/register.vue"),
+	},
+
+	{
+		path: "/dashboard",
+		name: "dashboard",
+		beforeEnter: auth,
+		component: () => import("../views/dashboard/AdminWelcome.vue"),
+		children: [
+			{
+				path: "",
+				name: "admin-welcome",
+				component: () => import("../views/dashboard/AdminWelcome.vue"),
+			},
+		],
+	},
+
+	{
+		path: "/user/profile",
+		name: "user-profile",
+		component: () => import("../views/dashboard/UserProfile.vue"),
 	},
 ];
 
